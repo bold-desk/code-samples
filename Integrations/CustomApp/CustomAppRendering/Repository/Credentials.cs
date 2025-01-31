5b2689e9ba9fdf6a2998c17a4e336ae951bc0f92
Narayanasamy J <30366719+narayanasamyj@users.noreply.github.com>
2025-01-31 10:01:18 +0530

    Customer app

---
+++ Integrations/CustomApp/CustomAppRendering/Repository/Credentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAppRendering.Repository
+{
+   /// <summary>
+   /// default properties for returning response
+   /// </summary>
+    public class Response
+    {
+        public int statusCode { get; set; }
+        public string message { get; set; }
+    }
+}
